82a1ea09ac405584521fefbef38f29a2193fb78c
LChinQuee <81491306+lchinquee@users.noreply.github.com>
2021-07-18 19:09:59 -0400

    update tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,11 +6,60 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    attributes: [
+      'id',
+      'tag_name'
+    ],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock']
+      },
+      {
+        model: ProductTag,
+        attributes: ['id']
+      }
+    ]
+  })
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err => {
+    if (err) throw err;
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'tag_name'
+    ],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock']
+      },
+      {
+        model: ProductTag,
+        attributes: ['id']
+      }
+    ]
+  })
+  .then(dbTagData => {
+    if (!dbTagData) {
+      res.status(404).json({ message: 'No tag found with this id' });
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch(err => {
+    if (err) throw err;
+  });
 });
 
 router.post('/', (req, res) => {
